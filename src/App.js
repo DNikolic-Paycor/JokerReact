2d6800b998af631b4b7a29b6a347fce02c2e6c5f
kicaubuntu <kicaubuntu@gmail.com>
2017-12-10 15:29:29 +0100

    filtering a code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-        joke:"",
-        answer:"",
+    this.state = {
+      joke: "",
+      answer: ""
     };
   }
 
-nextJoke(){
-    fetch('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke')
-     .then((result) => {
-       return result.json();
+  nextJoke() {
+    fetch('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke').then((result) => {
+      return result.json();
 
-     }).then((data1)=>{
-        this.setState({joke:data1.setup});
-        this.setState({answer:data1.punchline})
-       }
-     )
+    }).then((data1) => {
+      this.setState({joke: data1.setup, answer: data1.punchline});
+    })
   }
-  componentWillMount(){
+
+  componentWillMount() {
     this.nextJoke();
-    }
+  }
 
   render() {
-    return (
-      <div className="App">
-        <div className="App-header align-center">
+    return (<div className="App">
+      <div className="App-header align-center">
         <h1>How about a Joke?</h1>
-          <p className="red-text">..dont forget to smile..</p>
-          </div>
-          <div className="App-joke align-center">
-            <h3>"{this.state.joke}"</h3>
-              <h3 className="align-center">{this.state.answer}</h3>
-              <button onClick={()=>this.nextJoke()}>More jokes..</button>
-              </div>
-
+        <p className="red-text">..dont forget to smile..</p>
       </div>
-    );
+      <div className="App-joke align-center">
+        <h3>"{this.state.joke}"</h3>
+        <h3 className="align-center">{this.state.answer}</h3>
+        <button onClick={() => this.nextJoke()}>More jokes..</button>
+      </div>
+
+    </div>);
   }
 }
 
